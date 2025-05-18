0686bd3ebfdda57e95aea8da1c61a9b749c3f8fc
Roman Buchynskyi <roman.buchynskyi2006@gmail.com>
2025-05-18 19:50:22 +0200

    Add register and login modal windows with responsive styles and open/close action scripts

---
+++ Foliofy/wwwroot/assets/js/modalPagesActions.js
@@ -0,0 +1,35 @@
+﻿const registerLink = document.getElementById("register-link");
+const modalRegister = document.querySelector(".modal-register");
+const closeButton = modalRegister.querySelector(".close-btn");
+const registerForm = modalRegister.querySelector(".register-form");
+
+const loginModal = document.querySelector(".modal-login");
+const close = loginModal.querySelector(".close-btn");
+const loginLink = document.querySelector("#login-link");
+const loginForm = loginModal.querySelector(".login-form");
+
+registerLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    modalRegister.classList.add("open");
+    document.body.classList.add("passive");
+});
+
+closeButton.addEventListener("click", () => {
+    modalRegister.classList.remove("open");
+    document.body.classList.remove("passive");
+    clearErrors(registerForm);
+});
+
+loginLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    loginModal.classList.add("open");
+    document.body.classList.add("passive");
+});
+
+close.addEventListener("click", () => {
+    loginModal.classList.remove("open");
+    document.body.classList.remove("passive");
+    clearErrors(loginForm);
+});
+
+
