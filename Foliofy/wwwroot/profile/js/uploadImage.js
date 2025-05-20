8cdb49a84e3bfbec47481ce1eb369e4c6f34f729
Roman Buchynskyi <roman.buchynskyi2006@gmail.com>
2025-05-20 22:27:52 +0200

    Add script which will open and save the icon in profile edit page

---
+++ Foliofy/wwwroot/profile/js/uploadImage.js
@@ -0,0 +1,20 @@
+﻿const uploadButton = document.querySelector(".change-icon-btn"); 
+const imageInput = document.querySelector("#UploadedIcon");    
+const profileIcon = document.querySelector("#iconPreview");     
+
+uploadButton.addEventListener("click", () => {
+    imageInput.click();
+});
+
+imageInput.addEventListener("change", () => {
+    const file = imageInput.files[0];
+
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.addEventListener("load", (e) => {
+        profileIcon.src = e.target.result;
+    });
+
+    fileReader.readAsDataURL(file);
+});
